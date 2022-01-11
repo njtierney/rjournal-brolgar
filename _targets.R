e1102ba296253ad8c0c94a437f56a8339b688078
Nicholas Tierney <nicholas.tierney@gmail.com>
2022-01-11 11:28:09 +1000

    update R and package versions

--- _targets.R
+++ _targets.R
@@ -15,16 +15,13 @@ options(clustermq.scheduler = "multicore")
 # Uncomment below to deploy targets to parallel jobs
 # on a Sun Grid Engine cluster when running tar_make_clustermq().
 # options(clustermq.scheduler = "sge", clustermq.template = "sge.tmpl")
-future::plan(future::multiprocess)
+future::plan(future::multisession)
 
 # pipeline ---------------------------------------------------------------------
-tar_pipeline(
-  tar_target(heights_brolgar,
-             prepare_heights(n_obs_filter = 5)),
-  tar_target(heights_gam,
-             fit_heights_gam(heights_brolgar)),
-  tar_target(model_time,
-             heights_gam$time_taken),
+tar_plan(
+  heights_brolgar = prepare_heights(n_obs_filter = 5),
+  heights_gam = fit_heights_gam(heights_brolgar),
+model_time = heights_gam$time_taken,
   # also need to add in a part that checks if a .bib file has been changed
   # perhaps using `tar_target(format = "file")`, 
   tar_target(paper_bib,
@@ -34,8 +31,6 @@ tar_pipeline(
              path = "paper/brolgar-paper.Rmd"),
   # ideally this would only run if the report has been rendered,
   # not sure how to detect that.
-  tar_target(arxiv_paper,
-             zip_for_arxiv()),
-  tar_target(rjournal,
-             zip_for_rjournal())
+  arxiv_paper = zip_for_arxiv(),
+  rjournal = zip_for_rjournal()
 )
